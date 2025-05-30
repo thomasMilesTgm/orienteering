89f4c62de64d550862473f173cd378fe53a63869
Tom Miles <tom@ai-nc.com>
2025-05-30 14:46:12 +1000

    wip: bevy example

--- examples/bevy.rs
+++ examples/bevy.rs
@@ -1,8 +1,27 @@
 //! Minimal example of rendering an [`orienteering::topography::WorldMap`] in [`bevy`].
 
+use bevy::{
+    DefaultPlugins,
+    app::{App, Startup},
+    ecs::{resource::Resource, system::Commands},
+};
 use orienteering::{proc_gen::*, topography::WorldMap};
 
 fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .init_resource::<WorldResource>()
+        .run();
+}
+
+#[derive(Resource, Default)]
+pub struct WorldResource {
+    pub seed: MapSeed,
+    pub map: Option<WorldMap>,
+}
+
+fn setup(mut commands: Commands) {
     let seed = MapSeed::default();
     let _map = WorldMap::new(seed);
 }
