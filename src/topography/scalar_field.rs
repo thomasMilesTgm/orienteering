e202d6632b5db635100491a3192b866ca000ba27
Tom Miles <tom@ai-nc.com>
2025-06-08 14:44:25 +1000

    wip

--- src/topography/scalar_field.rs
+++ src/topography/scalar_field.rs
@@ -1,6 +1,7 @@
 //! Using scalar fields guarantees the resulting vector field is conservative, guaranteeing that
 //! the line integral of a point to itself is zero, which is a requirement for closed contours.
 
+use enum_dispatch::enum_dispatch;
 use nalgebra::{Point2, Vector2};
 
 pub trait ScalarField {
@@ -11,6 +12,7 @@ pub trait ScalarField {
     fn v_xy(&self, xy: Point2<f32>) -> Vector2<f32>;
 }
 
+#[enum_dispatch]
 pub trait DifferentiableFn {
     /// Calculates the value of the function at time `t`, i.e., `f(t)`
     fn f(&self, t: f32) -> f32;
@@ -25,7 +27,7 @@ pub trait DifferentiableFn {
 
     /// Apply the product rule, calculating `d/dt[f(t) * g(t)]`
     fn product<T: DifferentiableFn>(&self, t: f32, g: T) -> f32 {
-        self.f(t) * g.df_dt(t) + self.df_dt(t) * g.f(t)
+        dbg!(self.f(t) * g.df_dt(t)) + dbg!(self.df_dt(t) * g.f(t))
     }
 
     /// Apply the quotient rule, calculating `d/dt[f(t) / g(t)]`, where `f` is `self` and `g` is
@@ -35,7 +37,88 @@ pub trait DifferentiableFn {
     }
 }
 
+pub struct Fn2D {
+    pub x: FnType,
+    pub y: FnType,
+}
+
+impl Fn2D {
+    pub fn f(&self, xy: Point2<f32>) -> f32 {
+        self.x.f(xy.x) + self.y.f(xy.y)
+    }
+
+    pub fn df_dx(&self, xy: Point2<f32>) -> f32 {
+        self.x.df_dt(xy.x) + self.y.df_dt(xy.y)
+    }
+}
+
+/// A product of [`FnType`]
+#[derive(Debug, Clone)]
+pub struct FnChain {
+    inner: Box<FnType>,
+    outer: Box<FnType>,
+}
+
+impl FnChain {
+    pub fn new(inner: FnType, outer: FnType) -> Self {
+        Self {
+            inner: Box::new(inner),
+            outer: Box::new(outer),
+        }
+    }
+}
+
+impl DifferentiableFn for FnChain {
+    fn f(&self, t: f32) -> f32 {
+        self.outer.f(self.inner.f(t))
+    }
+
+    fn df_dt(&self, t: f32) -> f32 {
+        self.outer.chain(t, *self.inner.clone())
+    }
+}
+
+/// A product of [`FnType`]
+#[derive(Debug, Clone)]
+pub struct FnProduct {
+    lhs: Box<FnType>,
+    rhs: Box<FnType>,
+}
+
+impl FnProduct {
+    pub fn new(lhs: FnType, rhs: FnType) -> Self {
+        Self {
+            lhs: Box::new(lhs),
+            rhs: Box::new(rhs),
+        }
+    }
+}
+
+impl DifferentiableFn for FnProduct {
+    fn f(&self, t: f32) -> f32 {
+        self.lhs.f(t) * self.rhs.f(t)
+    }
+
+    fn df_dt(&self, t: f32) -> f32 {
+        self.rhs.product(t, *self.lhs.clone())
+    }
+}
+
+#[enum_dispatch(DifferentiableFn)]
+#[derive(Debug, Clone)]
+pub enum FnType {
+    Constant(Constant),
+    Exp(Exp),
+    Linear(Linear),
+    Log(Log),
+    Power(Power),
+    Sin(Sin),
+    Product(FnProduct),
+    Chain(FnChain),
+}
+
 /// `f(t) = c`
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Constant(pub f32);
 
 impl DifferentiableFn for Constant {
@@ -49,6 +132,7 @@ impl DifferentiableFn for Constant {
 }
 
 /// `f(t) = e^t`
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Exp;
 
 impl DifferentiableFn for Exp {
@@ -61,23 +145,23 @@ impl DifferentiableFn for Exp {
 }
 
 /// `f(t) = c * t`
-pub struct Linear {
-    pub k: f32,
-}
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Linear;
 
 impl DifferentiableFn for Linear {
     fn f(&self, t: f32) -> f32 {
-        self.k * t
+        t
     }
     fn df_dt(&self, _: f32) -> f32 {
-        self.k
+        1.
     }
 }
 
 /// `f(t) = ln(t)` (natural logarithm)
-pub struct Ln;
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Log;
 
-impl DifferentiableFn for Ln {
+impl DifferentiableFn for Log {
     fn f(&self, t: f32) -> f32 {
         t.ln()
     }
@@ -88,6 +172,7 @@ impl DifferentiableFn for Ln {
 }
 
 /// Power `f(t) = t^k`.
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Power {
     pub k: f32,
 }
@@ -103,6 +188,7 @@ impl DifferentiableFn for Power {
 }
 
 /// `f(t) = sin(t)`
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Sin;
 
 impl DifferentiableFn for Sin {
@@ -112,5 +198,27 @@ impl DifferentiableFn for Sin {
 
     fn df_dt(&self, t: f32) -> f32 {
         -t.cos()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_fn_product() {
+        let f1 = FnType::Constant(Constant(2.0));
+
+        assert_eq!(f1.f(1.0), 2.0);
+        assert_eq!(f1.df_dt(1.0), 0.0);
+
+        let f2 = FnType::Linear(Linear);
+
+        assert_eq!(f2.f(1.0), 1.0);
+        assert_eq!(f2.df_dt(1.0), 1.0);
+
+        let product = FnProduct::new(f1, f2);
+
+        assert_eq!(product.f(1.0), 2.0);
+        assert_eq!(product.df_dt(1.0), 2.0);
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,32 +1,5 @@
 //! Various utilities.
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum Dir {
-    X,
-    Y,
-}
-
-impl Dir {
-    pub fn r(&self, pt: &Point2<f32>) -> f32 {
-        match self {
-            Dir::X => pt.x,
-            Dir::Y => pt.y,
-        }
-    }
-    pub fn to_pt(&self, value: f32) -> Point2<f32> {
-        match self {
-            Dir::X => Point2::new(value, 0.),
-            Dir::Y => Point2::new(0., value),
-        }
-    }
-    pub fn to_vector(&self, value: f32) -> Vector2<f32> {
-        match self {
-            Dir::X => Vector2::new(value, 0.),
-            Dir::Y => Vector2::new(0., value),
-        }
-    }
-}
-
 #[allow(non_camel_case_types)]
 pub type of32 = ordered_float::OrderedFloat<f32>;
 
