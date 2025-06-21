fef7edbe04a6a2a6b1dd576c8a76a7bc6d7bdd12
Tom Miles <tom@ai-nc.com>
2025-06-21 11:56:32 +1000

    fix: step region function

--- src/calculus.rs
+++ src/calculus.rs
@@ -31,6 +31,7 @@ pub trait DifferentiableFn {
 #[derive(Debug, Clone)]
 pub enum FnType {
     Step(StepFn),
+    StepRegion(StepRegion),
     Constant(Constant),
     Exp(Exp),
     Linear(Linear),
@@ -42,6 +43,27 @@ pub enum FnType {
     Product(FnProduct),
     Quotient(FnQuotient),
     Sum(FnSum),
+    Sub(FnSub),
+}
+impl std::fmt::Display for FnType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FnType::Step(fn_step) => write!(f, "{}", fn_step),
+            FnType::StepRegion(fn_region) => write!(f, "{}", fn_region),
+            FnType::Constant(c) => write!(f, "{}", c),
+            FnType::Exp(exp) => write!(f, "{}", exp),
+            FnType::Linear(linear) => write!(f, "{}", linear),
+            FnType::Log(log) => write!(f, "{}", log),
+            FnType::Power(power) => write!(f, "{}", power),
+            FnType::Sin(sin) => write!(f, "{}", sin),
+            FnType::Cos(cos) => write!(f, "{}", cos),
+            FnType::Chain(chain) => write!(f, "{}", chain),
+            FnType::Product(product) => write!(f, "{}", product),
+            FnType::Quotient(quotient) => write!(f, "{}", quotient),
+            FnType::Sum(sum) => write!(f, "{}", sum),
+            FnType::Sub(sub) => write!(f, "{}", sub),
+        }
+    }
 }
 
 impl FnType {
@@ -73,16 +95,31 @@ impl FnType {
         FnType::Cos(Cos)
     }
 
-    pub fn product_of(lhs: FnType, rhs: FnType) -> Self {
+    pub fn product_of<L: Into<FnType>, R: Into<FnType>>(lhs: L, rhs: R) -> Self {
         FnType::Product(FnProduct::new(lhs, rhs))
     }
 
-    pub fn chain(outer: FnType, inner: FnType) -> Self {
+    pub fn chain<L: Into<FnType>, R: Into<FnType>>(outer: L, inner: R) -> Self {
         FnType::Chain(FnChain::new(inner, outer))
     }
 
-    pub fn quotient_of(lhs: FnType, rhs: FnType) -> Self {
+    pub fn quotient_of<L: Into<FnType>, R: Into<FnType>>(lhs: L, rhs: R) -> Self {
         FnType::Quotient(FnQuotient::new(lhs, rhs))
+    }
+
+    pub fn sum_of<L: Into<FnType>, R: Into<FnType>>(lhs: L, rhs: R) -> Self {
+        FnType::Sum(FnSum::new(lhs, rhs))
+    }
+    pub fn subtract<L: Into<FnType>, R: Into<FnType>>(lhs: L, rhs: R) -> Self {
+        FnType::Sub(FnSub::new(lhs, rhs))
+    }
+
+    pub fn step() -> Self {
+        FnType::Step(StepFn)
+    }
+
+    pub fn step_region(start: f32, end: f32, k: f32) -> Self {
+        FnType::StepRegion(StepRegion::new(start, end, k))
     }
 }
 
@@ -115,11 +152,21 @@ pub struct FnChain {
     outer: Box<FnType>,
 }
 
+impl std::fmt::Display for FnChain {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let outer = format!("{}", self.outer);
+        let inner = format!("{}", self.inner);
+        let fo_in = outer.replace("%", &inner);
+
+        write!(f, "{}", fo_in)
+    }
+}
+
 impl FnChain {
-    pub fn new(inner: FnType, outer: FnType) -> Self {
+    pub fn new<L: Into<FnType>, R: Into<FnType>>(inner: L, outer: R) -> Self {
         Self {
-            inner: Box::new(inner),
-            outer: Box::new(outer),
+            inner: Box::new(inner.into()),
+            outer: Box::new(outer.into()),
         }
     }
 }
@@ -145,11 +192,17 @@ pub struct FnQuotient {
     rhs: Box<FnType>,
 }
 
+impl std::fmt::Display for FnQuotient {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}/{}", self.lhs, self.rhs)
+    }
+}
+
 impl FnQuotient {
-    pub fn new(lhs: FnType, rhs: FnType) -> Self {
+    pub fn new<L: Into<FnType>, R: Into<FnType>>(lhs: L, rhs: R) -> Self {
         Self {
-            lhs: Box::new(lhs),
-            rhs: Box::new(rhs),
+            lhs: Box::new(lhs.into()),
+            rhs: Box::new(rhs.into()),
         }
     }
 }
@@ -166,9 +219,44 @@ impl DifferentiableFn for FnQuotient {
 }
 
 #[derive(Debug, Clone)]
+pub struct FnSub {
+    lhs: Box<FnType>,
+    rhs: Box<FnType>,
+}
+impl std::fmt::Display for FnSub {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({} - {})", self.lhs, self.rhs)
+    }
+}
+
+impl DifferentiableFn for FnSub {
+    fn f(&self, t: f32) -> f32 {
+        self.lhs.f(t) - self.rhs.f(t)
+    }
+    fn df_dt(&self, t: f32) -> f32 {
+        self.lhs.df_dt(t) - self.rhs.df_dt(t)
+    }
+}
+
+impl FnSub {
+    pub fn new<L: Into<FnType>, R: Into<FnType>>(lhs: L, rhs: R) -> Self {
+        Self {
+            lhs: Box::new(lhs.into()),
+            rhs: Box::new(rhs.into()),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct FnSum {
     lhs: Box<FnType>,
     rhs: Box<FnType>,
+}
+
+impl std::fmt::Display for FnSum {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({} + {})", self.lhs, self.rhs)
+    }
 }
 
 impl DifferentiableFn for FnSum {
@@ -181,10 +269,10 @@ impl DifferentiableFn for FnSum {
 }
 
 impl FnSum {
-    pub fn new(lhs: FnType, rhs: FnType) -> Self {
+    pub fn new<L: Into<FnType>, R: Into<FnType>>(lhs: L, rhs: R) -> Self {
         Self {
-            lhs: Box::new(lhs),
-            rhs: Box::new(rhs),
+            lhs: Box::new(lhs.into()),
+            rhs: Box::new(rhs.into()),
         }
     }
 }
@@ -196,11 +284,17 @@ pub struct FnProduct {
     rhs: Box<FnType>,
 }
 
+impl std::fmt::Display for FnProduct {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({} * {})", self.lhs, self.rhs)
+    }
+}
+
 impl FnProduct {
-    pub fn new(lhs: FnType, rhs: FnType) -> Self {
+    pub fn new<L: Into<FnType>, R: Into<FnType>>(lhs: L, rhs: R) -> Self {
         Self {
-            lhs: Box::new(lhs),
-            rhs: Box::new(rhs),
+            lhs: Box::new(lhs.into()),
+            rhs: Box::new(rhs.into()),
         }
     }
 }
@@ -220,6 +314,12 @@ impl DifferentiableFn for FnProduct {
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct StepFn;
 
+impl std::fmt::Display for StepFn {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "(0.5 + 0.5 * tanh(%))")
+    }
+}
+
 impl DifferentiableFn for StepFn {
     fn f(&self, t: f32) -> f32 {
         0.5 + 0.5 * t.tanh()
@@ -230,9 +330,59 @@ impl DifferentiableFn for StepFn {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct StepRegion {
+    pub start: f32,
+    pub end: f32,
+    pub k: f32,
+    func: Box<FnType>,
+}
+
+impl std::fmt::Display for StepRegion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.func)
+    }
+}
+
+impl StepRegion {
+    pub fn new(start: f32, end: f32, k: f32) -> Self {
+        let t_minus_start = FnSub::new(Constant(start), Linear); // (start - t)
+        let k_x_t_minus_start = FnProduct::new(Constant(k), t_minus_start); // k * (start - t)
+        let step_up = FnChain::new(k_x_t_minus_start, StepFn); // 0.5 + 0.5 * tanh(k(start - t))
+
+        let t_minus_end = FnSub::new(Constant(end), Linear); // (end - t)
+        let k_x_t_minus_end = FnProduct::new(Constant(k), t_minus_end); // k * (end - t)
+        let step_down = FnChain::new(k_x_t_minus_end, StepFn); // 0.5 + 0.5 * tanh(k(end - t))
+
+        let f = FnType::subtract(step_down, step_up);
+
+        Self {
+            start,
+            end,
+            k,
+            func: Box::new(f),
+        }
+    }
+}
+
+impl DifferentiableFn for StepRegion {
+    fn f(&self, t: f32) -> f32 {
+        self.func.f(t)
+    }
+    fn df_dt(&self, t: f32) -> f32 {
+        self.func.df_dt(t)
+    }
+}
+
 /// `f(t) = c`
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Constant(pub f32);
+
+impl std::fmt::Display for Constant {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 impl DifferentiableFn for Constant {
     fn f(&self, _: f32) -> f32 {
@@ -248,6 +398,12 @@ impl DifferentiableFn for Constant {
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Exp;
 
+impl std::fmt::Display for Exp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "e^%")
+    }
+}
+
 impl DifferentiableFn for Exp {
     fn f(&self, t: f32) -> f32 {
         t.exp()
@@ -257,9 +413,15 @@ impl DifferentiableFn for Exp {
     }
 }
 
-/// `f(t) = c * t`
+/// `f(t) = t`
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Linear;
+
+impl std::fmt::Display for Linear {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "%")
+    }
+}
 
 impl DifferentiableFn for Linear {
     fn f(&self, t: f32) -> f32 {
@@ -273,6 +435,12 @@ impl DifferentiableFn for Linear {
 /// `f(t) = ln(t)` (natural logarithm)
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Log;
+
+impl std::fmt::Display for Log {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "ln(%))")
+    }
+}
 
 impl DifferentiableFn for Log {
     fn f(&self, t: f32) -> f32 {
@@ -288,6 +456,12 @@ impl DifferentiableFn for Log {
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Power(pub f32);
 
+impl std::fmt::Display for Power {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "%^{}", self.0)
+    }
+}
+
 impl DifferentiableFn for Power {
     fn f(&self, t: f32) -> f32 {
         t.powf(self.0)
@@ -302,6 +476,12 @@ impl DifferentiableFn for Power {
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Sin;
 
+impl std::fmt::Display for Sin {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "sin(%))")
+    }
+}
+
 impl DifferentiableFn for Sin {
     fn f(&self, r: f32) -> f32 {
         r.sin()
@@ -315,6 +495,12 @@ impl DifferentiableFn for Sin {
 /// `f(t) = cos(t)`
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Cos;
+
+impl std::fmt::Display for Cos {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "cos(%))")
+    }
+}
 
 impl DifferentiableFn for Cos {
     fn f(&self, r: f32) -> f32 {
@@ -338,7 +524,7 @@ impl DifferentiableFn for Cos {
 #[macro_export]
 macro_rules! chain {
     ($inner:expr => $outer:expr) => {
-        FnType::chain($outer.into(), $inner.into())
+        FnType::chain($outer, $inner)
     };
 }
 
@@ -467,5 +653,45 @@ mod tests {
             assert_eq!(sin.f(t), f_expect(t));
             assert_eq!(sin.df_dt(t), df_expect(t));
         }
+    }
+
+    #[test]
+    fn test_step() {
+        let step = FnType::step();
+
+        approx::assert_relative_eq!(step.f(-1000.), 0.);
+        approx::assert_relative_eq!(step.f(-100.), 0.);
+        approx::assert_relative_eq!(step.f(-10.), 0.);
+        approx::assert_relative_eq!(step.f(0.), 0.5);
+        approx::assert_relative_eq!(step.f(10.), 1.);
+        approx::assert_relative_eq!(step.f(100.), 1.);
+        approx::assert_relative_eq!(step.f(1000.), 1.);
+    }
+
+    #[test]
+    fn test_step_region() {
+        const START: f32 = -10.;
+        const END: f32 = 20.;
+        const K: f32 = 40.;
+        let step = FnType::step_region(START, END, K);
+        println!("Step Region: {step}");
+
+        const N: usize = 100;
+        let ext = END - START;
+        let dt = ext / N as f32;
+        let t0 = START - ext / 2.;
+        let t1 = END + ext / 2.;
+        let dt = (t1 - t0) / N as f32;
+        for i in 0..N {
+            let t = t0 + dt * i as f32;
+            let f = step.f(t);
+            println!("f({t}) = {f}");
+        }
+
+        approx::assert_relative_eq!(0., step.f(START - 1.));
+        approx::assert_relative_eq!(1., step.f(START + 1.));
+        approx::assert_relative_eq!(1., step.f((END - START) / 2.));
+        approx::assert_relative_eq!(1., step.f(END - 1.));
+        approx::assert_relative_eq!(0., step.f(END + 1.));
     }
 }
